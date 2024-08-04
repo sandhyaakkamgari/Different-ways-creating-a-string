4adf86f574d0d347209ed6d8141bfe122e605d02
sandhya <145090559+sandhyaakkamgari@users.noreply.github.com>
2024-08-04 11:45:19 +0530

    Add files via upload

---
+++ definingstring.py
@@ -0,0 +1,10 @@
+my_string = 'Hello, world!' #using single quotes
+
+my_string = "Hello, world!" #using double quotes
+
+my_string = '''This is a
+multi-line
+string.'''   #Using Triple Quotes
+
+num = 42
+my_string = str(num)  # Converts the integer 42 to the string "42" #Using the str() Constructor
